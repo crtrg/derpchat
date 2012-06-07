217e53994dc551cddb2ba21aa32133ef88d8fccc
Nick Gauthier <ngauthier@gmail.com>
2012-06-07 14:29:11 -0400

    derpchat is go

---
+++ server.js
@@ -0,0 +1,9 @@
+var io = require('socket.io').listen(5000);
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
