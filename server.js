a4d8a813b490c30ac933d5e1333e204b758b97b3
Adam Bachman <adam.bachman@gmail.com>
2012-06-07 15:32:11 -0400

    shut it down

--- server.js
+++
@@ -1,9 +0,0 @@
-var io = require('socket.io').listen(5000);
-
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
-
